67172ea36f7d17727c930f69492ab5dd7ea09a95
Shuaihu-git <1583139786@qq.com>
2022-10-08 16:24:22 +0800

    5、foreach标签     collection:设置要循环的数组或集合     item:用一个字符串表示数组或集合中的每一个数据     separator：设置每次循环数据之间的分割符     open：循环的所有内容以什么开始     close：训话的所有内容以什么结束     6、sql片段标签     可以记录一段sql语句，在需要的地方可以用include引用进来使用它     <sql id="EmpColumns" >         emp_id,emp_name,age,sex,dept_id     </sql>     <include refid="EmpColumns"></include>     以及模糊删除

--- MyBatis_DynamicMapper/src/main/java/vip/shuaihu/www/mapper/EmpMapper.java
+++ MyBatis_DynamicMapper/src/main/java/vip/shuaihu/www/mapper/EmpMapper.java
@@ -1,10 +1,41 @@
 package vip.shuaihu.www.mapper;
 
+import org.apache.ibatis.annotations.Param;
 import vip.shuaihu.www.pojo.Emp;
 
 import java.util.List;
 
 public interface EmpMapper {
+    /**
+     * 动态SQl
+     * @param emp
+     * @return
+     */
     List<Emp> getListByDynamic(Emp emp);
+
+    /**
+     * 使用Choose标签查询员工信息
+     * @param emp
+     * @return
+     */
     List<Emp> getListByChoose(Emp emp);
+
+    /**
+     * 使用foreach标签批量添加数据
+     * @param emps
+     */
+    void InsertUsersByBatch(@Param("emps") List<Emp> emps);
+
+    /**
+     * 使用foreach标签批量删除员工信息
+     * @param empids
+     */
+    void deleteUsersByBatch(@Param("empids") Integer[] empids);
+
+    /**
+     * 模糊删除
+     * @param empnames
+     * @return
+     */
+    int deleteUsersByLike(@Param("empnames")String empnames);
 }
--- MyBatis_DynamicMapper/src/test/java/TestEmpMapper.java
+++ MyBatis_DynamicMapper/src/test/java/TestEmpMapper.java
@@ -4,7 +4,9 @@ import vip.shuaihu.www.mapper.EmpMapper;
 import vip.shuaihu.www.pojo.Emp;
 import vip.shuaihu.www.utils.SqlSessionUtils;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class TestEmpMapper {
     @Test
@@ -14,12 +16,50 @@ public class TestEmpMapper {
         Emp emp=new Emp(null,"",null,"男");
         List<Emp> listByDynamic = mapper.getListByDynamic(emp);
         listByDynamic.forEach(System.out::println);
-    }@Test
+    }
+    @Test
     public void testEmpMapperByChoose(){
         SqlSession session = SqlSessionUtils.getSession();
         EmpMapper mapper = session.getMapper(EmpMapper.class);
         Emp emp=new Emp(null,"",null,"男");
         List<Emp> listByDynamic = mapper.getListByChoose(emp);
         listByDynamic.forEach(System.out::println);
+    }
+    @Test
+    public void testInsertEmpBybatch(){
+        SqlSession session = SqlSessionUtils.getSession();
+        EmpMapper mapper = session.getMapper(EmpMapper.class);
+        List<Emp> emps=new ArrayList<>();
+        Emp emp = new Emp(null,"张三", 22,  "男" );
+        emps.add(emp);
+        for (int i = 0; i < 20; i++) {
+            String s = new String(new StringBuffer("++u513").append(i)).replace("++","");
+            int anInt = new Random().nextInt(25);
+            Emp emp1 = new Emp(null,s+i, anInt>0?anInt:22, i == i * 2 ? "男" : "女");
+            emps.add(emp1);
+        }
+        mapper.InsertUsersByBatch(emps);
+        emps.forEach(System.out::println);
+    }
+    @Test
+    public void testDeleteEmpBybatch(){
+        SqlSession session = SqlSessionUtils.getSession();
+        EmpMapper mapper = session.getMapper(EmpMapper.class);
+        Integer[] integers=new Integer[]{18,19,20,21,22,23,24,25,26,27,28,29};
+        mapper.deleteUsersByBatch(integers);
+
+    }
+    @Test
+    public void testDeleteEmpByLike(){
+        SqlSession session = SqlSessionUtils.getSession();
+        EmpMapper mapper = session.getMapper(EmpMapper.class);
+        int i = mapper.deleteUsersByLike("1");
+        System.out.println(i);
+
+    }
+    @Test
+    public void test(){
+        String s="\u5532";
+        System.out.println(s);
     }
 }
